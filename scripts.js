38e40ff74d02fdb8fc02db08a95b386ca995bd49
yudithleon <yudithkleon@gmail.com>
2025-11-14 20:03:29 -0500

    conectando con js

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const tbody = document.getElementById("todo-body");
+const alerta = document.getElementById("alerta");
+
+let tareas = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const valor = input.value.trim();
+
+  if (!valor) return;
+
+  const id = Date.now();
+  tareas.push({ valor, id });
+  input.value = "";
+
+  console.log(tareas)
+//   mostrar();
+});
+
+const mostrar = () => {
+  tbody.innerHTML = "";
+
+  tareas.forEach((ele, index) => {
+    console.log(ele.value, index)
+    // const tr = document.createElement("tr");
+
+    // tr.innerHTML = `
+    //   <th scope="row">${index + 1}</th>  
+    //   <td>${ele.valor}</td>
+    //   <td><button class="btn btn-danger btn-sm eliminar-btn" data-id="${ele.id}">
+    //       Eliminar
+    //   </button></td>
+    // `;
+
+    // tbody.appendChild(tr); 
+  });
+};
+
+
